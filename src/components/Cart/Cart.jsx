3d08d14650cbc4a305d89190de693f4a4ab2e66a
RefatRingky <rifatsourav43@gmail.com>
2023-04-06 03:04:19 +0600

    ema jhon devlope with react router

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,11 +7,13 @@ const Cart = ({cart}) => {
     
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
 
     for (const product of cart) {
        
       totalPrice = totalPrice + product.price;
       totalShipping = totalPrice + product.shipping
+      quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -38,7 +38,19 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // cart.push(product); 
-        const newCart = [...cart, product];
+        let newCart = [];
+       
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart= [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+       newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id)
     }
